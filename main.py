a9b840a958b05460583473c55f49a55922a5165d
gabrysera <gabrieleserafini10100@gmail.com>
2021-12-03 11:18:58 +0100

    cmt

--- main.py
+++ main.py
@@ -3,24 +3,34 @@ crease_array = list(map(str,input()))
 
 def opposite(s, i, j):
     for n in range(i-1, j - 1, -1):
-        if n-1 >= 0 and n-1 < len(s) and i+(i-n)-1 >= 0 and i+(i-n)-1 < len(s):
-            if s[n-1] == s[i+(i-n)-1]:
-                return False
+        if s[n-1] == s[i+(i-n)-1]:
+            return False
     return True
 
 
 def find_min_fold(s):
-    n = len(s) 
+    n = len(s) + 1
     fold = [n] * n
     fold[0] = 0
     fold[1] = 1
-    for i in range(2,len(s)):
+    for i in range(2,len(s)+1):
         new_min = fold[i-1] + 1
         for j in range(i-1,0,-1):
-            if (i-j >= n - i):
+            if (i-j+i<n):
                 if  opposite(s, i, j):
                     new_min = min(new_min, fold[j-1]+1)    
             fold[i] = new_min
-    return fold[-1]
+    return fold
 
-print(find_min_fold(crease_array))
+def find_optimal_fold(fold1, fold2):
+    min_sum = min(fold1[-1],fold2[0])
+    for i in range(int(len(fold1)/2),len(fold1)-1):
+        min_sum = min(min_sum, fold1[i]+fold2[i+1])
+    print(min_sum)
+
+
+fold1 = find_min_fold(crease_array)[1:]
+print(fold1)
+fold2 = find_min_fold(crease_array[::-1])[::-1][:-1]
+print(fold2)
+print(find_optimal_fold(fold1, fold2))
