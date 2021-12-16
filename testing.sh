706ebeecba107903c3784401ce06a1d0e339677e
dineshk.l <dineshk.l@protonmail.com>
2021-12-16 17:50:00 +0100

    changes

--- testing.sh
+++ testing.sh
@@ -4,6 +4,6 @@ for value in {1..41}
 		gen=$(cat test_cases/Folding-samples/$value.in | python main.py)
 		exp=$(cat test_cases/Folding-samples/$value.out)
 		if [ "$gen" != "$exp" ]; then
-			echo "Test case $value doesn't work. Expected: $exp ; Generated: $gen ";
+			echo "Test case $value doesn't work. Expected: $exp - Generated: $gen ";
 		fi
 	done
