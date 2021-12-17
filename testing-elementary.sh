202164f7a94be2b15edd0161131d84c288d0b8b5
dineshk.l <dineshk.l@protonmail.com>
2021-12-17 13:33:38 +0100

    number of tests update

--- testing-elementary.sh
+++ testing-elementary.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-for value in {1..41}
+for value in {1..508}
 	do
 		gen=$(cat test-cases/Folding-samples/elementary-tests/$value.in | python main.py)
 		exp=$(cat test-cases/Folding-samples/elementary-tests/$value.out)
 		if [ "$gen" != "$exp" ]; then
-			echo -e "Test case $value doesn't work. Expected: $exp: Generated: $gen\n";
+			echo -e "Test case $value doesn't work. Expected: $exp: Generated: $gen";
 		fi
 	done
